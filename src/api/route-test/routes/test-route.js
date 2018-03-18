14c88b5ffa4aa98a40c7cad3e55aeeaee5dd1c7d
Kim Carter <Kim.Carter@binarymist.net>
2018-03-18 15:55:12 +1300

    /test-route now returns results of cucumber test Made /test-route asynchronous Converted fs.readFile to return promise

--- src/api/route-test/routes/test-route.js
+++ src/api/route-test/routes/test-route.js
@@ -3,7 +3,7 @@
 module.exports = {
   method: 'POST',
   path: '/test-route',
-  handler: (request, respToolkit) => { // eslint-disable-line no-unused-vars
+  handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
     const { model } = request.server.app;
     // Todo: KC: Pass config to model..
     model.initialise('I am a dummy config');
@@ -14,28 +14,37 @@ module.exports = {
       Object.keys(require.cache).forEach(key => delete require.cache[key])
     }
 
-    // https://github.com/cucumber/cucumber-js/issues/786
-    // Details for forking and spawning are:
-    //    https://nodejs.org/api/child_process.html
-    //    https://medium.freecodecamp.org/node-js-child-processes-everything-you-need-to-know-e69498fe970a
-    //    And this one for debugging the additional child process: https://github.com/nodejs/node/issues/8690
-
     let args = []
       .concat(['node'])
       .concat([`${process.cwd}/node_modules/.bin/cucumber-js`]);
     
     let cucumberCli = new cucumber.Cli({argv: args.concat(['src/features', '-r', 'src/steps', '--exit', `--format=json:${process.cwd()}/test/security/report.txt`]), cwd: process.cwd(), stdout: process.stdout});
 
-    cucumberCli.run()
-      .then((succeeded) => {
-        console.log(succeeded);
-        // clearSupportCodeFns();
-        clearRequireCache();
-      }).catch((error) => {
-      console.log(error);
-      });
+    let resp;
 
-    return 'test-route handler';
+    // https://robbg.io/blog/2017-03-31-async-await-and-node-fs/
+    // http://2ality.com/2017/05/util-promisify.html
+    const fs = require('fs');
+    const { promisify } = require('util');
+    const readFileAsync = promisify(fs.readFile);
+
+    await cucumberCli.run()
+    .then((succeeded) => {
+      console.log(succeeded);
+      // clearSupportCodeFns();
+      clearRequireCache();
+        
+    }).catch((error) => {
+      console.log(error);
+    });
+    
+    await readFileAsync(`${process.cwd()}/test/security/report.txt`, {encoding: 'utf8'})
+    .then((fileText) => {
+      resp = respToolkit.response(fileText);
+    
+    });
+
+    return `test-route handler. ${resp.source}`;
   }
 };
 
