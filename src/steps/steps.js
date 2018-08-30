cb539ff72bae758343dc14240cdf9f9c7aee259a
Kim Carter <Kim.Carter@binarymist.net>
2018-08-30 12:17:00 +1200

    Linted src/steps/steps.js

--- src/steps/steps.js
+++ src/steps/steps.js
@@ -2,14 +2,14 @@
 const { Given, When, Then } = require('cucumber');
 const { expect } = require('code');
 
-Given('a variable set to {int}', function(number) {
-  this.setTo(number)
-})
+Given('a variable set to {int}', (number) => {
+  this.setTo(number);
+});
 
-When('I increment the variable by {int}', function(number) {
-  this.incrementBy(number)
-})
+When('I increment the variable by {int}', (number) => {
+  this.incrementBy(number);
+});
 
-Then('the variable should contain {int}', function(number) {
+Then('the variable should contain {int}', (number) => {
   expect(this.variable).to.equal(number);
-})
+});
