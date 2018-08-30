fdb54ee028c2775a209d857c6cedba8193f4fa6c
Kim Carter <Kim.Carter@binarymist.net>
2018-08-30 12:18:02 +1200

    Linted src/steps/world.js

--- src/steps/world.js
+++ src/steps/world.js
@@ -1,22 +1,20 @@
-const config = require(`${process.cwd()}/config/config`);
+const config = require(`${process.cwd()}/config/config`); // eslint-disable-line import/no-dynamic-require
 const log = require('purpleteam-logger').init(config.get('logger'));
 // features/support/world.js
 const cucumber = require('cucumber');
+
 const { setWorldConstructor, setDefaultTimeout } = cucumber;
-const ZapClient = require('zaproxy');
-const sut = require(`${process.cwd()}/src/api/app/do/sut`);
-const zap = require(`${process.cwd()}/src/slaves/zap`);
 
-
-let testStepResult;
+const sut = require(`${process.cwd()}/src/api/app/do/sut`); // eslint-disable-line import/no-dynamic-require
+const zap = require(`${process.cwd()}/src/slaves/zap`); // eslint-disable-line import/no-dynamic-require
 
 
 class CustomWorld {
-  constructor({attach, parameters}) {
+  constructor({ attach, parameters }) {
     const { sutProperties } = parameters;
 
     this.log = log;
-    this.log.notice(`Constructing the cucumber world for session with id "${parameters.sutProperties.testSession.id}".\n`, {tags: ['world']});
+    this.log.notice(`Constructing the cucumber world for session with id "${parameters.sutProperties.testSession.id}".\n`, { tags: ['world'] });
 
     this.variable = 0;
     this.attach = attach;
@@ -24,9 +22,9 @@ class CustomWorld {
     setDefaultTimeout(parameters.cucumber.timeOut);
 
     this.sut = sut;
-    this.sut.init({log, sutProperties});
+    this.sut.init({ log, sutProperties });
     this.zap = zap;
-    this.zap.init({log, slaveProperties: { ...parameters.slaveProperties, sutBaseUrl: this.sut.baseUrl() } } );
+    this.zap.init({ log, slaveProperties: { ...parameters.slaveProperties, sutBaseUrl: this.sut.baseUrl() } });
   }
 
 
@@ -38,12 +36,12 @@ class CustomWorld {
   // simple_math related stuff.
 
   setTo(number) {
-    this.variable = number
+    this.variable = number;
   }
 
   incrementBy(number) {
-    this.variable += number
+    this.variable += number;
   }
 }
 
-setWorldConstructor(CustomWorld)
+setWorldConstructor(CustomWorld);
