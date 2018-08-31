aaa0f49633d5f71a703380a7cc3a09d2d9401669
Kim Carter <Kim.Carter@binarymist.net>
2018-09-01 11:29:07 +1200

    Linted src/api/app/routes/post.js

--- src/api/app/routes/post.js
+++ src/api/app/routes/post.js
@@ -1,12 +1,10 @@
-
-
 module.exports = [{
   method: 'POST',
   path: '/test-plan',
   handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
     const { model } = request.server.app;
     const { payload: testJob } = request;
-    
+
     return respToolkit.response(await model.testPlan(testJob));
   }
 }, {
@@ -15,10 +13,8 @@ module.exports = [{
   handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
     const { model } = request.server.app;
     const { payload: testJob } = request;
-    
+
     return respToolkit.response(await model.runJob(testJob));
-
-
   }
 }];
 
@@ -27,4 +23,3 @@ module.exports = [{
 
 
 // Return low levvel report for orchestrator to compile with other testers and format.
-
