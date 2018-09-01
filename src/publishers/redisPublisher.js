fa9d6030bbe02654d9894922d55ffda8debf31dd
Kim Carter <Kim.Carter@binarymist.net>
2018-09-01 12:33:26 +1200

    Linted src/publishers/redisPublisher.js

--- src/publishers/redisPublisher.js
+++ src/publishers/redisPublisher.js
@@ -4,22 +4,23 @@ let log;
 let client;
 const baseChannel = 'app';
 
-const init = (options) => {
-  log = options.log;
-  client = redis.createClient(options.redis);
-  return { publish };
-};
-
 
 const publish = (sessionId, data) => {
   const channel = `${baseChannel}${sessionId ? `-${sessionId}` : ''}`;
   try {
-    log.debug(`Redis client publishing to the channel: "${channel}".`, { tags: ['redisPublisher'] })
+    log.debug(`Redis client publishing to the channel: "${channel}".`, { tags: ['redisPublisher'] });
     client.publish(channel, data);
   } catch (e) {
     log.warning(`The redis client failed to publish to the channel: "${channel}". The error was: ${e}`, { tags: ['redisPublisher'] });
     throw e;
   }
+};
+
+
+const init = (options) => {
+  ({ log } = options);
+  client = redis.createClient(options.redis);
+  return { publish };
 };
 
 
