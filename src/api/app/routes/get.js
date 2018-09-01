5490ef7c6596a883c7b60a4243f2da4ae18c0c07
Kim Carter <Kim.Carter@binarymist.net>
2018-09-01 12:07:57 +1200

    Linted src/api/app/routes/get.js

--- src/api/app/routes/get.js
+++ src/api/app/routes/get.js
@@ -1,39 +1,22 @@
-
-
 module.exports = [{
   method: 'GET',
   path: '/test-results',
   handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
-
-
-
-
     const { model } = request.server.app;
-    
-    
+
+
     // Todo: restResult obviously may not be ready now, so need to deal with this.
-    testResult = respToolkit.response(await model.testResult());
-
-
-
-
-
-
-
-
+    const testResult = respToolkit.response(await model.testResult());
 
 
     // Send results
 
-    const response = respToolkit.event( { event: 'appTestingResult', id: 4, data: { testingResult: 'test results comming soon' } } );
+    const response = respToolkit.event({ event: 'appTestingResult', id: 4, data: { testingResult: 'test results comming soon' } });
 
-    setTimeout( () => {
-      
-      respToolkit.event( { event: 'appTestingResult', id: 2, data: { testingResult: testResult.source } } )
+    setTimeout(() => {
+      respToolkit.event({ event: 'appTestingResult', id: 2, data: { testingResult: testResult.source } });
     }, 5000);
 
     return response;
-
   }
 }];
-
