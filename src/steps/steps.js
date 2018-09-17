4d08c18fbeef06644a8c9bcf903c6784cc5700c3
Kim Carter <Kim.Carter@binarymist.net>
2018-09-17 15:18:40 +1200

    Fixed bugs in linted steps/steps.js

--- src/steps/steps.js
+++ src/steps/steps.js
@@ -2,14 +2,17 @@
 const { Given, When, Then } = require('cucumber');
 const { expect } = require('code');
 
-Given('a variable set to {int}', (number) => {
+// Cucumber expects a non arrow function in order for the this to refer to the world.
+/* eslint-disable func-names */
+Given('a variable set to {int}', function (number) {
   this.setTo(number);
 });
 
-When('I increment the variable by {int}', (number) => {
+When('I increment the variable by {int}', function (number) {
   this.incrementBy(number);
 });
 
-Then('the variable should contain {int}', (number) => {
+Then('the variable should contain {int}', function (number) {
   expect(this.variable).to.equal(number);
 });
+/* eslint-enable func-names */
