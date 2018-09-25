0336a381cccb0f6e79e9104d8368caabc9c6b79a
Kim Carter <Kim.Carter@binarymist.net>
2018-09-26 10:36:07 +1200

    Tried reusing same support code library for sequential cucumber run: [200~https://github.com/cucumber/cucumber-js/issues/786#issuecomment-422305867

--- src/api/app/models/app.sequential.js
+++ src/api/app/models/app.sequential.js
@@ -1,34 +1,51 @@
 const cucumber = require('cucumber');
 
-// This is just verifying end-to-end comms
-const sequential = (runParams) => {
-  const { model, model: { createCucumberArgs, publisher }, sessionsProps } = runParams;
-
-  // For testing single session. Cucumber won't run twice in the same process.
-
-  const cucumberArgs = createCucumberArgs.call(model, sessionsProps[1]);
+let publisher;
+let library;
 
 
+const createCucCli = async (cucArgs, sessionProps) => {
   const cucumberCliStdout = {
     publisher,
     write(...writeParams) {
       const [str] = writeParams;
-      publisher.pubLog({ testSessionId: sessionsProps[1].testSession.id, logLevel: 'notice', textData: str, tagObj: { tags: ['app.sequential', 'cucumberCLI-stdout-write'] } });
+      publisher.pubLog({ testSessionId: sessionProps.testSession.id, logLevel: 'notice', textData: str, tagObj: { tags: ['app.sequential', 'cucumberCLI-stdout-write'] } });
     }
   };
 
   const cucumberCliInstance = new cucumber.Cli({
-    argv: ['node', ...cucumberArgs],
+    argv: ['node', ...cucArgs],
     cwd: process.cwd(),
     stdout: cucumberCliStdout
   });
 
-  model.slavesDeployed = true;
-  // If you want to debug the tests before execution returns, uncomment the await, make this function async and add await to the calling function.
-  /* await */cucumberCliInstance.run()
-    .then(async (succeeded) => {
-      this.log.notice(`Output of cucumberCli after test run: ${JSON.stringify(succeeded)}.`, { tags: ['app'] });
-    }).catch(error => this.log.error(error, { tags: ['app'] }));
+  if (!library) {
+    const configuration = await cucumberCliInstance.getConfiguration();
+    library = cucumberCliInstance.getSupportCodeLibrary(configuration);
+  }
+
+  cucumberCliInstance.getSupportCodeLibrary = () => library;
+  return cucumberCliInstance;
+};
+
+
+const sequential = async (runParams) => {
+  const { model, model: { createCucumberArgs }, sessionsProps } = runParams;
+  ({ model: { publisher } } = runParams);
+
+  // For testing single session. Cucumber won't run twice in the same process.
+
+  for (const sessionProps of sessionsProps) { // eslint-disable-line no-restricted-syntax
+    const cucumberArgs = createCucumberArgs.call(model, sessionProps);
+    const cucumberCliInstance = await createCucCli(cucumberArgs, sessionProps);
+  
+    model.slavesDeployed = true;
+    // If you want to debug the tests before execution returns, uncomment the await, make this function async and add await to the calling function.
+    /* await */cucumberCliInstance.run()
+      .then(async (succeeded) => {
+        this.log.notice(`Output of cucumberCli after test run: ${JSON.stringify(succeeded)}.`, { tags: ['app'] });
+      }).catch(error => this.log.error(error, { tags: ['app'] }));
+  }
 };
 
 module.exports = sequential;
