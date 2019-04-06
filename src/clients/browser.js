e9fe38aff5ee688bde9f9db9642a13896417a1b3
Kim Carter <Kim.Carter@binarymist.net>
2019-04-06 18:37:17 +1300

    Added testSessionId to log messages

--- src/clients/browser.js
+++ src/clients/browser.js
@@ -15,23 +15,23 @@ const findElementThenClick = async (searchText, testSessionId, expectedLogInResp
   const authenticatedFeedback = async () => (expectedLogInResponse ? ` User was ${await authenticated(expectedLogInResponse) ? 'authenticated' : '***not*** authenticated, check the login credentials you supplied in the buildUserConfig'}.` : '');
   try {
     await driver.findElement(By.id(searchText)).click();
-    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using id="${searchText}", and clicked it.${await authenticatedFeedback()}`, tagObj: { tags: ['browser'] } });
+    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using id="${searchText}", and clicked it.${await authenticatedFeedback()} For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using id="${searchText}".`, tagObj: { tags: ['browser'] } });
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using id="${searchText}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   }
   try {
     await driver.findElement(By.className(searchText)).click();
-    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using className="${searchText}", and clicked it.${await authenticatedFeedback()}`, tagObj: { tags: ['browser'] } });
+    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using className="${searchText}", and clicked it.${await authenticatedFeedback()} For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using className="${searchText}".`, tagObj: { tags: ['browser'] } });
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using className="${searchText}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   }
   try {
     await driver.findElement(By.name(searchText)).click();
-    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using name="${searchText}", and clicked it.${await authenticatedFeedback()}`, tagObj: { tags: ['browser'] } });
+    return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using name="${searchText}", and clicked it.${await authenticatedFeedback()} For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using name="${searchText}".`, tagObj: { tags: ['browser'] } });
-    publisher.pubLog({ testSessionId, logLevel: 'crit', textData: `Unable to locate element using id, className, or name of "${searchText}".`, tagObj: { tags: ['browser'] } });
-    throw new Error(`Unable to locate element using id, className, or name of "${searchText}".`);
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using name="${searchText}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
+    publisher.pubLog({ testSessionId, logLevel: 'crit', textData: `Unable to locate element using id, className, or name of "${searchText}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
+    throw new Error(`Unable to locate element using id, className, or name of "${searchText}". For test session with id: "${testSessionId}".`);
   }
 };
 
@@ -40,27 +40,27 @@ const findElementThenSendKeys = async (attackField, testSessionId) => {
   try {
     if (attackField && attackField.visible) {
       await driver.findElement(By.id(attackField.name)).sendKeys(attackField.value);
-      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using id="${attackField.name}", and sent keys.`, tagObj: { tags: ['browser'] } });
+      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using id="${attackField.name}", and sent keys. For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
     }
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using id="${attackField.name}".`, tagObj: { tags: ['browser'] } });
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using id="${attackField.name}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   }
   try {
     if (attackField && attackField.visible) {
       await driver.findElement(By.className(attackField.name)).sendKeys(attackField.value);
-      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using className="${attackField.name}", and sent keys.`, tagObj: { tags: ['browser'] } });
+      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using className="${attackField.name}", and sent keys. For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
     }
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using className="${attackField.name}".`, tagObj: { tags: ['browser'] } });
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using className="${attackField.name}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
   }
   try {
     if (attackField && attackField.visible) {
       await driver.findElement(By.name(attackField.name)).sendKeys(attackField.value);
-      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using name="${attackField.name}", and sent keys.`, tagObj: { tags: ['browser'] } });
+      return publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Located element using name="${attackField.name}", and sent keys. For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
     }
   } catch (e) {
-    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using name="${attackField.name}".`, tagObj: { tags: ['browser'] } });
-    throw new Error(`Unable to locate element using id, className, or name of "${attackField.name}".`);
+    publisher.pubLog({ testSessionId, logLevel: 'notice', textData: `Unable to locate element using name="${attackField.name}". For test session with id: "${testSessionId}".`, tagObj: { tags: ['browser'] } });
+    throw new Error(`Unable to locate element using id, className, or name of "${attackField.name}". For test session with id: "${testSessionId}".`);
   }
   return ''; // Keep eslint happy
 };
